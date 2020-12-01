44bf5d3ba46e9e64633c3b91a28c5686d457af96
Fouad AlKada <fouad.alkada@dynatrace.com>
2020-12-01 06:26:27 +0100

    change baseURL to include /movies so that it correctly work with the k8s ingress

--- webapp/src/api/movies.js
+++ webapp/src/api/movies.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import {Constants} from "../Constants";
 
 const instance = axios.create({
-    baseURL: Constants.BaseURL
+    baseURL: `${Constants.BaseURL}/movies`
 });
 
 export default instance;
